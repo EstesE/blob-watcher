e998d231dc790a87a4b21e35aa22839afce13ce6
Mithrilhall <eric_estes@aspensquare.com>
2023-08-28 11:37:40 -0400

    Initial

---
+++ BlobTrigger/index.js
@@ -0,0 +1,175 @@
+// const azure = require('azure-storage');
+// const fs = require('fs');
+
+// // Azure Blob Storage connection string and container name
+// const connectionString = 'queue_STORAGE';
+// const containerName = 'z-image-processing';
+
+// // Create a BlobServiceClient to work with the blob storage
+// const blobService = azure.createBlobService(connectionString);
+
+'use strict';
+require("dotenv").config();
+const path = require("path");
+const config = require("config");
+// const { BlobServiceClient } = require("@azure/storage-blob");
+const { BlobServiceClient, StorageSharedKeyCredential, BlobHTTPHeaders } = require("@azure/storage-blob");
+const fs = require('fs');
+const sharp = require('sharp');
+const imagemin = require('imagemin');
+const guetzli = require('imagemin-guetzli');
+
+const azuriteHost = "localhost"; // Azurite host
+const azuritePort = 10000;       // Azurite port
+const accountName = "devstoreaccount1";
+const accountKey = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
+
+const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
+
+const blobServiceClient = new BlobServiceClient(`http://${azuriteHost}:${azuritePort}/${accountName}`, sharedKeyCredential);
+
+async function deleteBlob(containerName, blobName) {
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blobClient = containerClient.getBlobClient(blobName);
+
+    try {
+        await blobClient.delete();
+        console.log(`Deleted blob "${blobName}" successfully.`);
+    } catch (error) {
+        console.error(`Error deleting blob "${blobName}":`, error);
+    }
+}
+
+async function readBlobToStream(containerName, blobName) {
+    try {
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        const blobClient = containerClient.getBlobClient(blobName);
+        const response = await blobClient.download();
+        const blobStream = response.readableStreamBody;
+
+        const resizedStream = blobStream.pipe(sharp().resize({ width: 300 }));
+
+        const fileStream = fs.createWriteStream("resized.jpg");
+
+        resizedStream.pipe(fileStream);
+
+        await new Promise((resolve) => {
+            resizedStream.on("end", resolve);
+        });
+
+        console.log('resized');
+    
+        // // Create a writable stream to save the blob data
+        // const fileStream = fs.createWriteStream("pxfuel.jpg");
+    
+        // // Pipe the blob stream to the file stream
+        // blobStream.pipe(fileStream);
+    
+        // // Wait for the stream to finish writing
+        // await new Promise((resolve) => {
+        //   blobStream.on("end", resolve);
+        // });
+    
+        // console.log("Blob image has been successfully downloaded to 'downloaded_image.jpg'");
+      } catch (error) {
+        console.error("Error downloading the blob image:", error);
+      }
+}
+
+async function writeBufferToBlob(containerName, blobName, buffer, originalSize) {
+    try {
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        const fileInfo = path.parse(blobName);
+        const destinationBlobClient = containerClient.getBlockBlobClient(`website-files/${fileInfo.name}${fileInfo.ext}`);
+        await destinationBlobClient.uploadData(buffer);
+        console.log(`>> ${blobName} compressed from ${originalSize.toFixed(2)} KB to ${(Buffer.byteLength(buffer) / 1024).toFixed(2)} KB`);
+    } catch (error) {
+        debugger;
+    }
+  }
+
+async function compress(containerName, blobName, originalSize) {
+    try {
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        const sourceBlobClient = containerClient.getBlobClient(`website-files/${blobName}`);
+        const file = await sourceBlobClient.downloadToBuffer();
+
+        console.log(`>> running ${blobName} through guetzli...`);
+        const transform = await imagemin.buffer(file, {
+            plugins: [
+                guetzli({ quality: 84 })
+            ]
+        });
+
+        await writeBufferToBlob(containerName, blobName, transform, originalSize);
+    } catch (error) {
+        // console.error("Error resizing and writing to the blob:", error);
+        // TODO: DELETE image
+        throw new Error(`Error compressing image: ${error}`);
+    }
+}
+
+async function resizeAndWriteToBlob(containerName, blobName, size) {
+    try {
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        const sourceBlobClient = containerClient.getBlobClient(`website-files/${blobName}`);
+
+        const fileInfo = path.parse(blobName);
+
+        const destinationBlobClient = containerClient.getBlockBlobClient(`website-files/${fileInfo.name}-${size}${fileInfo.ext}`);
+        const file = await sourceBlobClient.downloadToBuffer();
+        const transform = sharp(file).resize({ width: size });
+
+        // const blobHTTPHeaders = BlobHTTPHeaders();
+        // blobHTTPHeaders.blobContentType = "image/jpeg";
+        
+        const uploadBlobResponse = await destinationBlobClient.uploadStream(transform);
+        console.log(`>> '${blobName}' has been resized and uploaded as '${fileInfo.name}-${size}${fileInfo.ext}' : blobResponse.requestId: ${uploadBlobResponse.requestId} `);
+        return Buffer.byteLength(file) / 1024;
+    } catch (error) {
+        console.error("Error resizing and writing to the blob:", error);
+    }
+}
+
+
+async function moveBlob(containerName, oldFolderPath, newFolderPath, blobName) {
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    // Create blob clients for the source and destination
+    const sourceBlobClient = containerClient.getBlobClient(`${oldFolderPath}/${blobName}`);
+    const destinationBlobClient = containerClient.getBlobClient(`${newFolderPath}/${blobName}`);
+
+    try {
+        // Start the copy operation from the source to the destination
+        const copyResponse = await destinationBlobClient.beginCopyFromURL(sourceBlobClient.url);
+
+        // Wait for the copy operation to complete
+        await copyResponse.pollUntilDone();
+
+        // Check if the copy operation was successful
+        if (copyResponse.operation.state.result.copyStatus === "success") {
+            // Delete the source blob (original image)
+            await sourceBlobClient.delete();
+            console.log(`>> Moved '${blobName}' from ${oldFolderPath} to ${newFolderPath}`);
+        } else {
+            console.error(`Copy operation failed: ${copyResponse.copyStatus}`);
+        }
+    } catch (error) {
+        console.error(`Error moving blob:`, error);
+    }
+}
+
+module.exports = async function (context, myBlob) {
+    context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
+
+    const containerName = "properties"
+    const blobName = context.bindingData.name;
+
+    // deleteBlob(containerName, blobName);
+    await moveBlob(`${containerName}`, 'uploads', 'website-files', blobName);
+    // await readBlobToStream(containerName, `website-files/${blobName}`);
+    const originalSize = await resizeAndWriteToBlob(containerName, blobName, 300);
+    await compress(containerName, blobName, originalSize);
+
+    console.log(`>> Finished processing '${context.bindingData.name}'`);
+};
